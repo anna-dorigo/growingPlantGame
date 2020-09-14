f2cf9471f522664f18101e92dc21bb4c63de3f8b
anna-dorigo <alejandr@gmail.com>
2020-09-13 22:39:36 -0400

    add feedPlant and growing functions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 // setting up global variables
-let canvas = document.getElementById("game-screen");
+var canvas = document.getElementById("game-screen");
 var ctx = canvas.getContext('2d');
 const weatherOptionsFileNames = ["./images/rainnyDay.png","./images/sunnyDay.png"];
 const weatherOptions = ["rainny", "sunny"];
@@ -20,7 +20,7 @@ ctx.fill();
 function initialSetUp(){
     alert("Welcome to the plant growing game!! \n"+
      "The idea is to find the right amount of water,"+
-     "light, and vitamins to give the plant to grow healthy for 10 days.\n\n"+
+     "light, and vitamins to give the plant to grow healthy for 5 days.\n\n"+
      "Click ok to start!");
 
     let todayWeather = Math.floor(Math.random() * 2);
@@ -38,7 +38,53 @@ function initialSetUp(){
     }
 }
 
-document.body.addEventListener("load",initialSetUp());
+function feedPlant(){
+    let lightValue = document.getElementById("light").value;
+    let waterValue = document.getElementById("water").value;
+    let vitaminsValue = document.getElementById("vitamins").value;
 
+    console.log("values "+ lightValue+"-"+waterValue+"-"+vitaminsValue);
+    console.log(currentWeather)
+
+    if(currentWeather === "rainny"){
+        if(waterValue < 4 && lightValue > 2 && vitaminsValue > 5 ){
+            plantGrow();
+            console.log("grow!! rainny");
+        }else{
+            //plantDie();
+            console.log("die!! rainny");
+        }
+    }
+
+    if(currentWeather === "sunny"){
+        if(waterValue > 4 && lightValue < 2 && vitaminsValue < 5 ){
+            plantGrow();
+            console.log("grow!! sunny");
+        }else{
+            //plantDie();
+            console.log("die!!sunny");
+        }
+    }
+
+    //reset inputs
+    document.getElementById("light").value = 1;
+    document.getElementById("water").value = 1;
+    document.getElementById("vitamins").value = 1;
+
+}
+
+function plantGrow(){
+        
+    console.log("growing")
+    ctx.beginPath();
+    ctx.moveTo(300, 525);
+    ctx.lineTo(300, 450);
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "#BED674";
+    ctx.stroke();
+
+}
+
+document.body.addEventListener("load",initialSetUp());
 let feedPlantBtn = document.getElementById("feed-plant-button");
 feedPlantBtn.addEventListener("click", feedPlant);
